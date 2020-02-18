fa6b4d6a8045118db69ab06b0d32e29a1368c547
juhász.ábel.kristóf <juhasz.abel99@gmail.com>
2020-02-18 13:45:18 +0100

    kiíró

--- biliárd/biliárd/Program.cs
+++ biliárd/biliárd/Program.cs
@@ -16,7 +16,25 @@ namespace biliárd
             {
                 Console.WriteLine(golyók[i]);
             }
+            int leeső = Leesősorsoló(golyók);
+            Kiíró(golyók);
             Console.ReadKey();
+        }
+
+        private static void Kiíró(List<int> golyók)
+        {
+            foreach (int item in golyók)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+
+        private static int Leesősorsoló(List<int> golyók)
+        {
+            Random sorsoló = new Random();
+            int szám = sorsoló.Next(golyók.Count);
+            golyók.Remove(szám);
+            return golyók[szám];
         }
 
         private static void Kezdés(List<int> golyók)
